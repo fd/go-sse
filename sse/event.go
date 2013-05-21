669e68051aac85e3c62a4f3785b1a8b981fdf5b4
Simon Menke <simon.menke@gmail.com>
2013-05-21 14:39:22 +0200

    First import

---
+++ sse/event.go
@@ -0,0 +1,64 @@
+package sse
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"time"
+)
+
+type Event struct {
+	Id    string
+	Type  string
+	Data  string
+	Retry time.Duration
+}
+
+func (e *Event) IsZero() bool {
+	return e.Id == "" && e.Type == "" && e.Data == "" && e.Retry == 0
+}
+
+func (e *Event) write_to(w io.Writer) error {
+	var (
+		err error
+	)
+
+	if e.IsZero() {
+		return nil
+	}
+
+	if e.Id != "" {
+		_, err = fmt.Fprintf(w, "id: %s\n", e.Id)
+		if err != nil {
+			return err
+		}
+	}
+
+	if e.Type != "" {
+		_, err = fmt.Fprintf(w, "event: %s\n", e.Type)
+		if err != nil {
+			return err
+		}
+	}
+
+	if e.Retry > 0 {
+		_, err = fmt.Fprintf(w, "retry: %d\n", e.Retry/time.Millisecond)
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, line := range strings.Split(e.Data, "\n") {
+		_, err = fmt.Fprintf(w, "data: %s\n", line)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = fmt.Fprint(w, "\n")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ sse/handler.go
@@ -0,0 +1,124 @@
+package sse
+
+import (
+	"bufio"
+	"io"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"time"
+)
+
+var keep_alive_payload = []byte(":keep-alive\n" +
+	":xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n" +
+	":xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n" +
+	":xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n" +
+	":xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n\n")
+
+type EventWriter interface {
+	Send(event *Event) error
+	Close() error
+}
+
+type event_writer_t struct {
+	conn         net.Conn
+	buf_writer   *bufio.ReadWriter
+	chunk_writer io.WriteCloser
+	events       chan send_event
+	closed       chan chan error
+}
+
+type send_event struct {
+	event *Event
+	reply chan error
+}
+
+func Hijack(w http.ResponseWriter) (EventWriter, error) {
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.WriteHeader(200)
+
+	conn, bufrw, err := w.(http.Hijacker).Hijack()
+	if err != nil {
+		return nil, err
+	}
+
+	bufrw.Flush()
+
+	ew := &event_writer_t{
+		conn:         conn,
+		buf_writer:   bufrw,
+		chunk_writer: httputil.NewChunkedWriter(bufrw),
+		events:       make(chan send_event),
+		closed:       make(chan chan error),
+	}
+
+	go ew.loop()
+
+	return ew, nil
+}
+
+func (ew *event_writer_t) Send(event *Event) error {
+	c := make(chan error)
+	ew.events <- send_event{event, c}
+	return <-c
+
+}
+func (ew *event_writer_t) Close() error {
+	c := make(chan error)
+	ew.closed <- c
+	return <-c
+}
+
+func (ew *event_writer_t) loop() {
+	var (
+		next_keep_alive = time.NewTicker(1 * time.Second)
+		zero            = make([]byte, 1)
+	)
+
+	for {
+		var (
+			now = time.Now()
+			err error
+		)
+
+		ew.conn.SetDeadline(now.Add(5 * time.Second))
+
+		select {
+
+		case req := <-ew.closed:
+			next_keep_alive.Stop()
+			ew.chunk_writer.Close()
+			ew.buf_writer.Flush()
+			ew.conn.Close()
+			close(ew.closed)
+			close(ew.events)
+			req <- nil
+			return
+
+		case req := <-ew.events:
+			err = req.event.write_to(ew.chunk_writer)
+			if err == io.EOF {
+				go ew.Close()
+			}
+
+			err = ew.buf_writer.Flush()
+			if err == io.EOF {
+				go ew.Close()
+			}
+			req.reply <- err
+
+		case <-next_keep_alive.C:
+			ew.conn.SetReadDeadline(now)
+			if _, err := ew.conn.Read(zero); err == io.EOF {
+				go ew.Close()
+			}
+
+			_, err = ew.chunk_writer.Write(keep_alive_payload)
+			if err == io.EOF {
+				go ew.Close()
+			}
+			ew.buf_writer.Flush()
+
+		}
+	}
+}
