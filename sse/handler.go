cedaacb07997e556dfcf87a8add7931450c9a9fe
Simon Menke <simon.menke@gmail.com>
2013-05-22 11:12:51 +0200

    Better detection of closed connections

--- sse/handler.go
+++ sse/handler.go
@@ -2,7 +2,6 @@ package sse
 
 import (
 	"fmt"
-	"io"
 	"net"
 	"net/http"
 	"sync"
@@ -14,6 +13,14 @@ var keep_alive_payload = []byte(":keep-alive\n" +
 	":xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n" +
 	":xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n" +
 	":xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\n\n")
+
+type ConnectionError struct {
+	Err error
+}
+
+func (err *ConnectionError) Error() string {
+	return fmt.Sprintf("sse: %s", err.Err.Error())
+}
 
 type EventWriter interface {
 	LastEventID() string
@@ -27,6 +34,7 @@ type event_writer_t struct {
 	conn            net.Conn
 	closed          chan bool
 	close_notifiers []chan bool
+	err             error
 	mtx             sync.RWMutex
 }
 
@@ -46,7 +54,7 @@ type write_event_res struct {
 func Hijack(w http.ResponseWriter, req *http.Request) (EventWriter, error) {
 	conn, _, err := w.(http.Hijacker).Hijack()
 	if err != nil {
-		return nil, err
+		return nil, &ConnectionError{err}
 	}
 
 	ew := &event_writer_t{
@@ -74,7 +82,8 @@ func (ew *event_writer_t) Write(event *Event) (int, error) {
 	ew.mtx.Lock()
 	defer ew.mtx.Unlock()
 
-	return event.write_to(ew.conn)
+	n, err := event.write_to(ew.conn)
+	return n, ew.set_err(err)
 }
 
 func (ew *event_writer_t) Close() error {
@@ -90,13 +99,7 @@ func (ew *event_writer_t) Close() error {
 		ew.closed = nil
 	}
 
-	for _, c := range ew.close_notifiers {
-		c <- true
-		close(c)
-	}
-	ew.close_notifiers = nil
-
-	return ew.conn.Close()
+	return ew.close()
 }
 
 func (ew *event_writer_t) CloseNotify() <-chan bool {
@@ -117,7 +120,6 @@ func (ew *event_writer_t) CloseNotify() <-chan bool {
 func (ew *event_writer_t) loop() {
 	var (
 		next_keep_alive = time.NewTicker(1 * time.Second)
-		zero            = make([]byte, 1)
 		closed          = ew.closed
 	)
 
@@ -130,7 +132,8 @@ func (ew *event_writer_t) loop() {
 			now = time.Now()
 		)
 
-		ew.conn.SetDeadline(now.Add(5 * time.Second))
+		ew.conn.SetReadDeadline(time.Time{})
+		ew.conn.SetWriteDeadline(now.Add(5 * time.Second))
 
 		select {
 
@@ -140,15 +143,14 @@ func (ew *event_writer_t) loop() {
 
 		case <-next_keep_alive.C:
 			// did the client hang up?
-			ew.conn.SetReadDeadline(time.Now())
-			if _, err := ew.conn.Read(zero); err == io.EOF {
-				// closed on remote side
-				ew.Close()
-			} else {
-				fmt.Printf("err: %T %s\n", err, err)
-				// send the keep alive
-				ew.conn.SetDeadline(now.Add(5 * time.Second))
-				ew.conn.Write(keep_alive_payload)
+			if err := ew.detect_closed_connection(); err != nil {
+				ew.close()
+				return
+			}
+
+			if err := ew.write_keep_alive(); err != nil {
+				ew.close()
+				return
 			}
 
 		}
@@ -166,5 +168,50 @@ func (ew *event_writer_t) write_response() error {
 	resp.Header.Set("Connection", "close")
 	resp.Header.Set("Date", time.Now().Format(http.TimeFormat))
 
-	return resp.Write(ew.conn)
+	return ew.set_err(resp.Write(ew.conn))
+}
+
+func (ew *event_writer_t) detect_closed_connection() error {
+	one := make([]byte, 1)
+
+	ew.conn.SetReadDeadline(time.Now())
+	_, err := ew.conn.Read(one)
+
+	if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
+		err = nil
+	}
+
+	if err != nil {
+		err = ew.set_err(err)
+		return err
+	}
+
+	ew.conn.SetReadDeadline(time.Time{})
+	return nil
+}
+
+func (ew *event_writer_t) write_keep_alive() error {
+	_, err := ew.conn.Write(keep_alive_payload)
+	return ew.set_err(err)
+}
+
+func (ew *event_writer_t) close() error {
+	for _, c := range ew.close_notifiers {
+		c <- true
+		close(c)
+	}
+	ew.close_notifiers = nil
+
+	return ew.set_err(ew.conn.Close())
+}
+
+func (ew *event_writer_t) set_err(err error) error {
+	if err != nil {
+		err = &ConnectionError{err}
+
+		if ew.err == nil {
+			ew.err = err
+		}
+	}
+	return err
 }
